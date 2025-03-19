590146b173f5690928fae7d66a6ad34d0eabacbb
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-19 12:51:06 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TerminalComponent } from './terminal/terminal.component';
+import { LoginComponent } from './login/login.component';
+import { ThemeSwitcherComponent } from './theme-switcher/theme-switcher.component';
+import { AuthService } from './services/auth.service';
+import { IdleService } from './services/idle.service';
+import { IdleOverlayComponent } from './idle-overlay/idle-overlay.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TerminalComponent],
-  templateUrl: './app.component.html',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TerminalComponent,
+    LoginComponent,
+    ThemeSwitcherComponent,
+    IdleOverlayComponent
+  ],
+  template: `
+    <app-theme-switcher></app-theme-switcher>
+    <app-login *ngIf="!(isAuthenticated$ | async)"></app-login>
+    <app-terminal *ngIf="isAuthenticated$ | async"></app-terminal>
+    <app-idle-overlay *ngIf="isIdle"></app-idle-overlay>
+  `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'matrix-portfolio';
+export class AppComponent implements OnInit {
+  isAuthenticated$ = this.authService.isAuthenticated$;
+  isIdle = false;
+
+  constructor(private authService: AuthService, private idleService: IdleService) {}
+
+  ngOnInit(): void {
+    this.idleService.idleState.subscribe(value => {
+      this.isIdle = value;
+    });
+  }
 }
